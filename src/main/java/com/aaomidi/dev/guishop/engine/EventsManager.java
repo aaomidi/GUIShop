e1a4a30f08807c2358208baafb16645196bfbe2e
Amir Omidi <amir@aaomidi.com>
2014-07-08 05:28:06 +0430

    I think this should fix it D:

--- src/main/java/com/aaomidi/dev/guishop/engine/EventsManager.java
+++ src/main/java/com/aaomidi/dev/guishop/engine/EventsManager.java
@@ -12,7 +12,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -37,6 +37,13 @@ public class EventsManager implements Listener {
             if (event.getCurrentItem() == null || event.getCurrentItem().getType() == Material.AIR || inventory.getItem(clickedSlot) == null || inventory.getItem(clickedSlot).getType() == Material.AIR) {
                 return;
             }
+            if (inventory.getHolder().equals(player)) {
+                return;
+            }
+            if (!event.getSlotType().equals(InventoryType.SlotType.CONTAINER)) {
+                event.setCancelled(true);
+            }
+
             if (inventory.getHolder() instanceof InventoryManager) {
                 GUICategory guiCategory = ConfigReader.getGuiCategories().get(clickedSlot);
                 guiCategory.onLeftClick(player);
@@ -64,13 +71,6 @@ public class EventsManager implements Listener {
         }
     }
 
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onInventoryClose(InventoryCloseEvent event) {
-        Player player = (Player) event.getPlayer();
-        if (Caching.getOpenInventoryMap().containsKey(player)) {
-            Caching.getOpenInventoryMap().remove(player);
-        }
-    }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPlayerChat(AsyncPlayerChatEvent event) {
